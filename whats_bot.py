193b0cf02b8aae841e059e8f4ce603bea411036c
Eduardo Leonardo <eduardo.rosa@tg4.com.br>
2021-02-06 22:26:15 -0300

    config whats

--- whats_bot.py
+++ whats_bot.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 import os
 from time import sleep
+import requests
+import json
 
 
 class zapbot:
@@ -24,10 +26,33 @@ class zapbot:
         # Aguarda alguns segundos para validação manual do QrCode
         self.driver.implicitly_wait(15)
 
+    def consultarPedido(self, pedido):
+        payload = {'VendaId': pedido, 'Integrador': 'SAV'}
+
+        headers = {'content-type': 'application/json'}
+        r = requests.post('http://localhost:5000/Status/ConsultaPedido',
+                          data=json.dumps(payload), headers=headers)
+        r.encoding = 'ISO-8859-1'
+        if r.status_code == 200:
+          bot.envia_msg(r.text)
+        else:
+          bot.envia_msg("Ocorreu um erro ao consulta o pedido")
+        
+
+
+    def reprocessaPedido(self, pedido):
+        payload = {'CodigoWooza': pedido}
+
+        headers = {'content-type': 'application/json'}
+        r = requests.post('http://localhost:5000/Status/ReprocessarPedido',
+                          data=json.dumps(payload), headers=headers)
+        r.encoding = 'ISO-8859-1'
+        bot.envia_msg(r.text)
+
     def ultima_msg(self):
         """ Captura a ultima mensagem da conversa """
         try:
-            post = self.driver.find_elements_by_class_name("_3_7SH")
+            post = self.driver.find_elements_by_class_name("_1wlJG")
             ultimo = len(post) - 1
             # O texto da ultima mensagem
             texto = post[ultimo].find_element_by_css_selector(
@@ -41,12 +66,14 @@ class zapbot:
         try:
             sleep(2)
             # Seleciona acaixa de mensagem
-            self.caixa_de_mensagem = self.driver.find_element_by_class_name("_2S1VP")
+            self.caixa_de_mensagem = self.driver.find_element_by_class_name(
+                "DuUXI")
             # Digita a mensagem
             self.caixa_de_mensagem.send_keys(msg)
-            sleep(1)
+            sleep(10)
             # Seleciona botão enviar
-            self.botao_enviar = self.driver.find_element_by_class_name("_35EW6")
+            self.botao_enviar = self.driver.find_element_by_xpath(
+                "//span[@data-icon='send']")
             # Envia msg
             self.botao_enviar.click()
             sleep(2)
@@ -57,14 +84,17 @@ class zapbot:
         """ Envia media """
         try:
             # Clica no botão adicionar
-            self.driver.find_element_by_css_selector("span[data-icon='clip']").click()
+            self.driver.find_element_by_css_selector(
+                "span[data-icon='clip']").click()
             # Seleciona input
-            attach = self.driver.find_element_by_css_selector("input[type='file']")
+            attach = self.driver.find_element_by_css_selector(
+                "input[type='file']")
             # Adiciona arquivo
             attach.send_keys(fileToSend)
             sleep(3)
             # Seleciona botão enviar
-            send = self.driver.find_element_by_xpath("//div[contains(@class, 'yavlE')]")
+            send = self.driver.find_element_by_xpath(
+                "//div[contains(@class, 'yavlE')]")
             # Clica no botão enviar
             send.click()
         except Exception as e:
@@ -73,13 +103,19 @@ class zapbot:
     def abre_conversa(self, contato):
         """ Abre a conversa com um contato especifico """
         try:
+            # //self.grupos_ou_pessoas = [contato]
+
             # Seleciona a caixa de pesquisa de conversa
-            self.caixa_de_pesquisa = self.driver.find_element_by_class_name("jN-F5")
+            # self.caixa_de_pesquisa = self.driver.find_element_by_class_name("jN-F5")
             # Digita o nome ou numero do contato
-            self.caixa_de_pesquisa.send_keys(contato)
-            sleep(2)
+            # self.caixa_de_pesquisa.send_keys(contato)
+            sleep(10)
             # Seleciona o contato
-            self.contato = self.driver.find_element_by_xpath("//span[@title = '{}']".format(contato))
+            # self.contato = self.driver.find_element_by_xpath("//span[@title = '{}']".format(contato))
+            self.contato = self.driver.find_element_by_xpath(
+                f"//span[@title='{contato}']")
+            sleep(2)
+
             # Entra na conversa
             self.contato.click()
         except Exception as e:
@@ -87,20 +123,26 @@ class zapbot:
 
 
 bot = zapbot()
-bot.abre_conversa("+55 11 99999-9999")
-bot.envia_msg("Olá, sou o bot whatsapp! Para receber ajuda digite: /help")
+bot.abre_conversa("WoozaHelp")
+# bot.consultarPedido("SA385199")
+# bot.envia_msg("Olá, sou o bot whatsapp! Para receber ajuda digite: /help")
 imagem = bot.dir_path + "/imagem.jpg"
 msg = ""
 while msg != "/quit":
-    sleep(1)
-    msg = bot.ultima_msg()
-    if msg == "/help":
-        bot.envia_msg("""Bot: Esse é um texto com os comandos válidos:
-            /help (para ajuda)
-            /mais (para saber mais)
-            /quit (para sair)
-            """)
-    elif msg == "/mais":
-        bot.envia_media(imagem)
-    elif msg == "/quit":
+    try:
+     sleep(1)
+     msg = bot.ultima_msg()
+     msgsplit = msg.split()
+     if msgsplit[0] == "Consultar" :
+        bot.consultarPedido(msgsplit[1])
+        # bot.envia_msg("""Bot: Esse é um texto com os comandos válidos:
+        #     /help (para ajuda)
+        #     /mais (para saber mais)
+        #     /quit (para sair)
+        #     """)
+     elif msgsplit[0] == "Reprocessar":
+        bot.reprocessaPedido(msgsplit[1])
+     elif msg == "/quit":
         bot.envia_msg("Bye bye!")
+    except Exception as e:
+      raise e
