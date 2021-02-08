3df359625359ebb2bc36f3592503e07d9570b889
Eduardo <eduardo.rosa@tg4.com.br>
2021-02-08 20:24:14 -0300

    Criando fila de msgs

--- whats_bot.py
+++ whats_bot.py
@@ -28,23 +28,69 @@ class zapbot:
 
     def consultarPedido(self):
        while True: 
-        if len(filaConsulta)  > 0 :
-          pedido = filaConsulta[0]
-          payload = {'VendaId': pedido, 'Integrador': 'SAV'}
+           try:
+              if len(filaConsulta)  > 0 :
+                pedido = filaConsulta[0]
+                payload = {'VendaId': pedido, 'Integrador': 'SAV'}
 
-          headers = {'content-type': 'application/json'}
-          r = requests.post('http://localhost:5000/Status/ConsultaPedido',
-                          data=json.dumps(payload), headers=headers)
-          r.encoding = 'ISO-8859-1'
-          if r.status_code == 200:
-            bot.envia_msg(r.text)
-            filaConsulta.popleft()
-          elif r.status_code == 202:
-            bot.envia_msg("Pedido não encontrado!")
-            filaConsulta.popleft()
-          else:
-            bot.envia_msg("Ocorreu um erro ao consultar o pedido!")
-            filaConsulta.popleft()
+                headers = {'content-type': 'application/json'}
+                r = requests.post('http://localhost:5000/Status/ConsultaPedido',
+                                data=json.dumps(payload), headers=headers)
+                r.encoding = 'ISO-8859-1'
+                if r.status_code == 200:
+                 filaMensagens.append({'demanda': r.text, 'tipo': 'msg'})
+                 filaEmEspera.append(pedido)
+                #   bot.envia_msg(r.text)
+                 filaConsulta.popleft()
+                elif r.status_code == 202:
+                #   bot.envia_msg("Pedido não encontrado!")
+                  filaMensagens.append({'demanda': "Pedido não encontrado!", 'tipo': 'msg'})
+                  filaEmEspera.append(pedido)
+                  filaConsulta.popleft()
+                elif r.status_code == 204:
+                #   bot.envia_msg("Pedido sem vendaId!")
+                  filaMensagens.append({'demanda': "Pedido sem vendaId!", 'tipo': 'msg'})
+                  filaEmEspera.append(pedido)
+                  filaConsulta.popleft()
+                else:
+                #   bot.envia_msg("Ocorreu um erro ao consultar o pedido!")
+                  filaMensagens.append({'demanda': "Ocorreu um erro ao consultar o pedido!", 'tipo': 'msg'})
+                  filaEmEspera.append(pedido)
+                  filaConsulta.popleft()
+           except Exception as e:
+            print("Erro ao consultar o servidor!")
+
+
+    def obterLog(self):
+       while True: 
+           try:
+              if len(filaLog)  > 0 :
+                pedido = filaLog[0]
+                headers = {'content-type': 'application/json'}
+                r = requests.get('http://localhost:5000/Log/ObterLog?codigoWooza='+ pedido,headers=headers)
+                r.encoding = 'ISO-8859-1'
+                if r.status_code == 200:
+                 filaMensagens.append({'demanda': "C:/Users/Dud/LogsIziPizi/LogsBot/Log-" + pedido+ ".txt", 'tipo': 'arquivo'})
+                #   bot.envia_arquivo("C:/Users/Dud/LogsIziPizi/LogsBot/Log-" + pedido+ ".txt")
+                 filaEmEspera.append(pedido)
+                 filaLog.popleft()
+                elif r.status_code == 202:
+                #   bot.envia_msg("Pedido não encontrado!")
+                 filaMensagens.append({'demanda': "Pedido não encontrado!", 'tipo': 'msg'})
+                 filaEmEspera.append(pedido)
+                 filaLog.popleft()
+                elif r.status_code == 204:
+                #   bot.envia_msg("Pedido sem vendaId!")
+                 filaMensagens.append({'demanda': "Pedido sem vendaId!", 'tipo': 'msg'})
+                 filaEmEspera.append(pedido)
+                 filaLog.popleft()
+                else:
+                #   bot.envia_msg("Ocorreu um erro ao consultar o pedido!")
+                 filaMensagens.append({'demanda': "Ocorreu um erro ao consultar o pedido!", 'tipo': 'msg'})
+                 filaEmEspera.append(pedido)
+                 filaLog.popleft()
+           except Exception as e:
+            print("Erro ao consultar o servidor!")              
         
 
 
@@ -59,13 +105,19 @@ class zapbot:
                                  data=json.dumps(payload), headers=headers)
                if r.status_code == 200:
                  r.encoding = 'ISO-8859-1'
-                 bot.envia_msg(r.text)
+                 filaMensagens.append({"demanda": r.text , "tipo": "msg"})
+                 filaEmEspera.append(pedido)
+                #  bot.envia_msg(r.text)
                  filaReprocessa.popleft()
                elif r.status_code == 202:
-                 bot.envia_msg("Pedido não encontrado!")
+                #  bot.envia_msg("Pedido não encontrado!")
+                 filaEmEspera.append(pedido)
+                 filaMensagens.append({'demanda': "Pedido não encontrado!", 'tipo': 'msg'})
                  filaReprocessa.popleft()
                else:
-                 bot.envia_msg("Ocorreu um erro ao reprocessar o pedido")
+                #  bot.envia_msg("Ocorreu um erro ao reprocessar o pedido")
+                 filaEmEspera.append(pedido)
+                 filaMensagens.append({'demanda': "Ocorreu um erro ao reprocessar o pedido", 'tipo': 'msg'})
                  filaReprocessa.popleft()                  
 
     def ultima_msg(self):
@@ -121,6 +173,51 @@ class zapbot:
         except Exception as e:
             print("Erro ao enviar media", e)
 
+    def envia_arquivo(self, fileToSend):
+        """ Envia media """
+        try:
+            self.envia_msg("Segue o log:")
+            # Clica no botão adicionar
+            self.driver.find_element_by_css_selector(
+                "span[data-icon='clip']").click()
+            # Seleciona input
+            attach = self.driver.find_element_by_css_selector(
+                "input[type='file']")
+            # Adiciona arquivo
+            attach.send_keys(fileToSend)
+            sleep(3)
+            # Seleciona botão enviar
+            send = self.driver.find_element_by_xpath(
+                "//div[contains(@class, '_3ipVb')]")
+            # Clica no botão enviar
+            send.click()
+        except Exception as e:
+            print("Erro ao enviar media", e)      
+
+
+    def enviar_msg_queu(self):
+      while True:
+        try:
+          if len(filaMensagens)  > 0:
+            demanda = filaMensagens[0]
+            if demanda['tipo']== "msg":
+              self.envia_msg(demanda['demanda'])
+              if len(filaConsulta) > 0:
+                 filaConsulta.popleft()
+              if len(filaReprocessa) > 0:      
+                 filaReprocessa.popleft()         
+            elif demanda['tipo']== "arquivo" :
+              self.envia_arquivo(demanda['demanda'])
+              filaLog.popleft()
+            else:
+                print("tipo inválido")
+            filaMensagens.popleft()
+            filaEmEspera.popleft()    
+        except Exception as e:
+           print("Erro ao tentar obter filaMensagens")
+
+
+
     def abre_conversa(self, contato):
         """ Abre a conversa com um contato especifico """
         try:
@@ -151,7 +248,7 @@ class zapbot:
            msg = bot.ultima_msg()
            msgsplit = msg.split()
            if msgsplit[0] == "Consultar" :
-               if msgsplit[1] not in filaConsulta:
+               if msgsplit[1] not in filaConsulta  and msgsplit[1] not in filaEmEspera:
                  filaConsulta.append(msgsplit[1])
               # bot.consultarPedido(msgsplit[1])
               # bot.envia_msg("""Bot: Esse é um texto com os comandos válidos:
@@ -160,9 +257,12 @@ class zapbot:
               #     /quit (para sair)
               #     """)
            elif msgsplit[0] == "Reprocessar":
-               if msgsplit[1] not in filaReprocessa:
+               if msgsplit[1] not in filaReprocessa and msgsplit[1] not in filaEmEspera :
                    filaReprocessa.append(msgsplit[1])
                 #  bot.reprocessaPedido(msgsplit[1])
+           elif msgsplit[0] == "ObterLog":
+               if msgsplit[1] not in filaLog and msgsplit[1] not in filaEmEspera:
+                  filaLog.append(msgsplit[1])
            elif msg == "/quit":
               bot.envia_msg("Bye bye!")
          except Exception as e:
@@ -178,9 +278,16 @@ imagem = bot.dir_path + "/imagem.jpg"
 msg = ""
 filaConsulta = deque([])
 filaReprocessa = deque([])
+filaLog = deque([])
+filaMensagens = deque([])
+filaEmEspera = deque([])
 t = Thread(target=bot.consultarPedido)
 t2 = Thread(target=bot.fluxo_tratamento)
 t3 = Thread(target=bot.reprocessaPedido)
+t4 = Thread(target=bot.obterLog)
+t5 = Thread(target=bot.enviar_msg_queu)
 t.start()
 t2.start()
 t3.start()
+t4.start()
+t5.start()
